5348b2dc2c97b2a00fc27f3e60d9d11f05021ec9
Nishant Guvvada <nishant.guvvada@gmail.com>
2025-12-30 11:01:30 +0530

    added dark bg

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 export const MainLayout = () => {
     return (
         <>
-            <main className="flex flex-col min-h-screen">
+            <main className="flex flex-col min-h-screen dark:bg-stone-800">
                 <Outlet />
             </main>
             <Footer/>
